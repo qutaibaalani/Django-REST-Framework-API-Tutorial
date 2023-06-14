0abed3f25cfbc436e9fce69e8414e9c9719259d6
qutaibaalani <qutaibaalani1984@gmail.com>
2023-06-14 13:51:39 -0400

    Worked on Relationships & Hyperlinked APIs

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
 from rest_framework import mixins
 from rest_framework import generics
 from snippets.models import Snippet
@@ -11,6 +13,16 @@ from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
 from rest_framework import permissions
 from snippets.permissions import IsOwnerOrReadOnly
+
+
+@api_view(["GET"])
+def api_root(request, format=None):
+    return Response(
+        {
+            "users": reverse("user-list", request=request, format=format),
+            "snippets": reverse("snippet-list", request=request, format=format),
+        }
+    )
 
 
 @api_view(["GET", "POST"])
@@ -55,6 +67,15 @@ def snippet_detail(request, pk, format=None):
     elif request.method == "DELETE":
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class SnippetList(APIView):
