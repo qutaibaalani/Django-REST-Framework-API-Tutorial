b8f1cc5655927ddd5381e834d2ec0135e0973ce9
qutaibaalani <qutaibaalani1984@gmail.com>
2023-06-14 11:43:59 -0400

    Worked on Requests and Responses

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -82,7 +82,10 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
